dfe37703baa3c68965e09d6eca916bab46a52f59
manojkumar3036 <shah.manoj86@gmail.com>
2018-06-08 16:06:51 +0530

    Added controller,dispatcher-servlet and viewResolver

---
+++ OnlineShopping/src/main/java/net/mks/OnlineShopping/controller/PageController.java
@@ -0,0 +1,18 @@
+package net.mks.OnlineShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","home","/index"})
+	public ModelAndView index()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to the Spring WebMvc");
+		return mv;
+	}
+
+}
